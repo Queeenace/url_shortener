bad36f4f8885714c910e4923b863eab533353806
Queeenace <zemskov.vladislav05@gmail.com>
2025-03-19 12:21:38 +0500

    Fixed a bug where user authorization did not affect the saving of link history

--- url_shortener/url_shortener/models.py
+++ url_shortener/url_shortener/models.py
@@ -2,7 +2,7 @@ from django.db import models
 import string
 import random
 from django.db.models import F
-
+from django.contrib.auth.models import User 
 
 class ShortenedURL(models.Model):
     original_url = models.URLField(
@@ -13,6 +13,7 @@ class ShortenedURL(models.Model):
         auto_now_add=True, verbose_name="Дата создания")
     click_count = models.PositiveIntegerField(
         default=0, verbose_name="Количество переходов")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='shortened_urls', null=True, blank=True)
 
     def save(self, *args, **kwargs) -> None:
         if not self.short_code:
@@ -28,9 +29,9 @@ class ShortenedURL(models.Model):
             if not ShortenedURL.objects.filter(short_code=code).exists():
                 return code
 
-    def increment_clicks(self) -> int:
-        self.update(click_count=F('click_count') + 1)
-        return self.click_count
+    def increment_clicks(self) -> None:
+        self.click_count = F('click_count') + 1
+        self.save()
 
     def __str__(self) -> str:
         return f"{self.short_code} -> {self.original_url} (Переходов: {self.click_count})"
